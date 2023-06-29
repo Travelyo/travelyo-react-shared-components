2f23c3619074cef00dd215247aab5460da677010
Ivan Ferenchuk <ivan.ferenchuk@gmail.com>
2023-06-29 15:59:06 +0300

    build

--- dist/cjs/types/components/icons/index.d.ts
+++ dist/cjs/types/components/icons/index.d.ts
@@ -1,3 +1,5 @@
 export { default as Hotel } from "./Hotel";
 export { default as ArrowDown } from "./ArrowDown";
 export { default as StarIcon } from "./StarIcon";
+export { default as WalkIcon } from './WalkIcon';
+export { default as CarIcon } from './CarIcon';
