43dba088e94d682a5cd1596eacc9dec9c660a70e
Ivan Ferenchuk <ivan.ferenchuk@gmail.com>
2025-02-18 17:52:48 +0100

    fix: gender type values

--- src/components/Proposal/components/AddClient.tsx
+++ src/components/Proposal/components/AddClient.tsx
@@ -20,11 +20,11 @@ const AddClient = (props: Props) => {
       <form className="flex flex-col gap-6 mb-20">
         <RadioGroup name="genderType" className='flex flex-row gap-4' defaultValue={state.clientForm.title} onValueChange={(value) => handleChange(value, 'genderType')}>
           <div className="flex items-center gap-2">
-            <RadioGroupItem value="mr" id="mr" />
+            <RadioGroupItem value="Mr" id="mr" />
             <Label htmlFor="mr">Mr</Label>
           </div>
           <div className="flex items-center gap-2">
-            <RadioGroupItem value="mrs" id="mrs" />
+            <RadioGroupItem value="Mrs" id="mrs" />
             <Label htmlFor="mrs">Mrs</Label>
           </div>
         </RadioGroup>
