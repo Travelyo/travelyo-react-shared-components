6105c16f41cede1c87df97c4e192fec64fb098cd
ferenchuk <ferenchuk@users.noreply.github.com>
2025-02-20 17:20:56 +0000

    Add new compiled assets

--- dist/types/components/Proposal/components/ProposalListItem.d.ts
+++ dist/types/components/Proposal/components/ProposalListItem.d.ts
@@ -3,6 +3,7 @@ import { Proposal } from '../ProposalTypes';
 type Props = {
     data: Proposal;
     onClick: (id: number) => void;
+    active: boolean;
 };
-declare const ProposalListItem: ({ data, onClick, }: Props) => React.JSX.Element;
+declare const ProposalListItem: ({ data, onClick, active, }: Props) => React.JSX.Element;
 export default ProposalListItem;
