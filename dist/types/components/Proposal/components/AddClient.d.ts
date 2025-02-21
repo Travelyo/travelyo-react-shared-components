404d7053bfbbca4d78f2c9c7d0115a4bb80b3a6e
ferenchuk <ferenchuk@users.noreply.github.com>
2025-02-21 17:16:13 +0000

    Add new compiled assets

--- dist/types/components/Proposal/components/AddClient.d.ts
+++ dist/types/components/Proposal/components/AddClient.d.ts
@@ -2,6 +2,7 @@ import React from 'react';
 type Props = {
     form: any;
     onChangeForm: (action: any) => void;
+    errors: any;
 };
-declare const AddClient: ({ form, onChangeForm, }: Props) => React.JSX.Element;
+declare const AddClient: ({ form, onChangeForm, errors: serverErrors, }: Props) => React.JSX.Element;
 export default AddClient;
--- dist/types/components/input/PhoneInputV2.d.ts
+++ dist/types/components/input/PhoneInputV2.d.ts
@@ -3,6 +3,7 @@ type Props = {
     initialCountry?: string;
     initialValue?: string;
     onChange: (value: string, name: string) => void;
+    error: string;
 };
-declare const PhoneInputV2: ({ initialCountry, initialValue, onChange, }: Props) => React.JSX.Element;
+declare const PhoneInputV2: ({ initialCountry, initialValue, onChange, error, }: Props) => React.JSX.Element;
 export default PhoneInputV2;
