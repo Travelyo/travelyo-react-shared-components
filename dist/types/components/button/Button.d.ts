2cb1a08edf62b9537b8145c95681c3ab8d37156b
ferenchuk <ferenchuk@users.noreply.github.com>
2025-02-26 11:05:55 +0000

    Add new compiled assets

--- dist/types/components/button/Button.d.ts
+++ dist/types/components/button/Button.d.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 type ButtonProps = {
-    label: string;
+    label: string | React.ReactNode;
     onClick?: () => void;
     variant?: 'primary' | 'secondary' | 'secondary-negative' | 'naked' | 'secondary-alt' | 'link';
     size?: 'small' | 'medium' | 'large' | 'huge';
@@ -9,6 +9,7 @@ type ButtonProps = {
     rounded?: boolean;
     inverted?: boolean;
     disabled?: boolean;
+    isLoading?: boolean;
 };
-declare const Button: ({ label, onClick, variant, size, rounded, inverted, className, disabled, ...props }: ButtonProps) => React.JSX.Element;
+declare const Button: ({ label, onClick, variant, size, rounded, inverted, className, disabled, isLoading, ...props }: ButtonProps) => React.JSX.Element;
 export default Button;
