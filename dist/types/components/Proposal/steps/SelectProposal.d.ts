12b0649ba56a8b23bd19294931e71b19c743e44c
ferenchuk <ferenchuk@users.noreply.github.com>
2025-02-27 15:47:19 +0000

    Add new compiled assets

--- dist/types/components/Proposal/steps/SelectProposal.d.ts
+++ dist/types/components/Proposal/steps/SelectProposal.d.ts
@@ -1,7 +1,8 @@
 import React from 'react';
+import { OfferData } from '../ProposalTypes';
 type Props = {
-    offerId: string;
+    offerData: OfferData;
     date: string;
 };
-declare const SelectProposal: ({ offerId, }: Props) => React.JSX.Element;
+declare const SelectProposal: ({ offerData, }: Props) => React.JSX.Element;
 export default SelectProposal;
